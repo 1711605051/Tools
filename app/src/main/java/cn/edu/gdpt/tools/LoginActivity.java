e0fc8e55974be531fd75c3990b2c7bc69ce6fa40
1711605051 <547164323@qq.com>
2019-06-19 10:18:00 +0800

    基本完成11

--- app/src/main/java/cn/edu/gdpt/tools/LoginActivity.java
+++ app/src/main/java/cn/edu/gdpt/tools/LoginActivity.java
@@ -1,7 +1,10 @@
 package cn.edu.gdpt.tools;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        findViewById(R.id.dl).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                finish();
+            }
+        });
     }
 }
